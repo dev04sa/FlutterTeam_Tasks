d6f490cb57ed2ef0a959b14b088c94dba9cada90
Devendra Singh <129666293+dev04sa@users.noreply.github.com>
2024-06-03 19:59:23 +0530

    Revert "CRUD App"

--- Task1/Aditya/crud_app_adi/lib/Utils/routes.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-class MyRoutes{
-  static String homePage = "/homePage";
-  static String loginPage = "/loginPage";
-  static String signUpPage = "/signUpPage";
-}
